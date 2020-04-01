c5160645d926ace832d4917be55f110776dda751
Mathias-Pc <mathias.rerup-dyberg@hotmail.com>
2020-04-01 11:04:15 +0200

    Hello world

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from kivy.uix.label import Label
 class MyApp(App):
 
     def build(self):
+        """Returns a label with the text "Hello world"""
+
         return Label(text='Hello world')
 
 
 if __name__ == '__main__':
     MyApp().run()
-

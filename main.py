1a318b0cb6313bc794f774bbddab5e8c25c2ac35
Emil Andersen <emilandersen001>
2020-04-14 13:03:01 +0200

    <

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ class MyApp(App):
 
     def build(self):
 
+
         """Returns a label with the text "Hello world"""
         #return Label(text='Hello world')
-        mapview = MapView(zoom=11, lat=56.040371, lon=12.579490)
+        mapview = MapView(zoom=30, lat=56.040371, lon=12.579490)
         return mapview
 
 if __name__ == '__main__':

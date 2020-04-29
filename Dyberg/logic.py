576f78b14f6fa92a1cfcacae39a3e936ab9423b5
Dyberg <mathias.rerup-dyberg@hotmail.com>
2020-04-29 11:21:03 +0200

    Added new stuff

--- Dyberg/logic.py
+++ Dyberg/logic.py
@@ -2,9 +2,59 @@ from Dyberg.databaseHandler import DBFunctions
 
 
 class Logic():
+    """Creating init function, where we can declare the otherr class, this class will be pulling from"""
     def __init__(self, parent=None):
         self.DBFunctions = DBFunctions(self) #Bliver ikke brugt pt
 
     def testLogic(self):
         DBFunctions.testPrint(self)
 
+    def FotoVognSpotted(self, lat, lon): #takes 2 argument, the gps koods
+        """What should it be doing?
+
+        1) Call Database with lat and lon, check if they are already there
+
+            If they aren't there, call add function from DBFunctions, which will add them, with the standards values
+            break
+
+            If they are in database, check how many times its been visited, then  add 1
+
+        2) If we dident break in 1), we check if the kods.active(its a bool)
+
+            if kods.active
+                :return
+
+            if not, check if kods.visited is higher than the minimum required (its a varialbe)
+
+            if it is higher, then change bool to true
+
+            if not
+                return
+        """
+
+    def PlaceFotoVogn(self):
+        """What should it be doing?
+
+        1) pull all Kods from database, using DBFunctions.get_all()
+
+        2) check if kods.active (bool)
+
+        3) if its active, it should be added to the mapview
+
+        self.foto = MapMarker(lat=Placeholder, lon=Placeholder)
+        self.mapview.add_marker(self.foto)
+
+        also append them to a list
+
+        4) call Alert with the active list
+
+        """
+
+    def Alert(self, ActiveKods, PersonLat, PersonLon):
+        """What should it be doing?
+
+        1) check if the personKods is within a sertain range, of the active kods
+
+        2) if it is, make the alarm button blink red, and play alarm
+
+        """
--- Dyberg/main.py
+++ Dyberg/main.py
@@ -2,36 +2,40 @@ from kivy.app import App
 from kivy.garden.mapview import MapView
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.garden.mapView import *
 from Dyberg.databaseHandler import DBFunctions
 from Dyberg.logic import Logic
 
 class MyApp(App):
 
-
-
-
+    """Building our app, normmally it would be the __init__ function"""
     def build(self): #Ville normalt være def __init__(self, parent=None):
 
         """Returns a label with the text "Hello world"""
         #return Label(text='Hello world')
+        """Creating MapView, which let us determ, zoom, lat and lon, essentiel it would be pulling from gps signlas"""
         self.mapview = MapView(zoom=11, lat=56.0394, lon=12.457)
         print(self.mapview.__dir__())
 
-        self.layout = BoxLayout(orientation="vertical")
-        self.button1 = Button(text="One")
+        self.foto = MapMarker(lat=56.0394, lon=12.457)
+        self.mapview.add_marker(self.foto)
 
+        """Making a layout, as a boxlayout, making it vertical to match our desired design"""
+        self.layout = BoxLayout(orientation="vertical")
+
+        """Initializing our buttons, then after connection them to functions, when they are pressed"""
+        self.button1 = Button(text="One")
         self.button1.bind(on_press=DBFunctions.testPrint)
 
         self.button2 = Button(text="Two")
-
         self.button2.bind(on_press=Logic.testLogic)
 
+        """Adding all the different stuff to our layout, in the desired order"""
         self.layout.add_widget(self.button1)
-
         self.layout.add_widget(self.mapview)
-
         self.layout.add_widget(self.button2)
 
+        """Returning the layout"""
         return self.layout
 
 if __name__ == '__main__':

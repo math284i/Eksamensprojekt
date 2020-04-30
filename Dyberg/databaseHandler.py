8e8e4bc971a2cef8ac6bbb0801537bf0d6ca9413
Dyberg <mathias.rerup-dyberg@hotmail.com>
2020-04-30 09:49:01 +0200

    Added new stuff

--- Dyberg/databaseHandler.py
+++ Dyberg/databaseHandler.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-cred = credentials.Certificate("Dyberg/eksamensproject-firebase-adminsdk-qsqlg-134386dbd0.json")
+cred = credentials.Certificate("eksamensproject-firebase-adminsdk-qsqlg-134386dbd0.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://eksamensproject.firebaseio.com/'
 })
@@ -15,18 +15,24 @@ class DBFunctions():
         print("Det virker")
 
     def get(self, koordinater):
-        self.ref = db.reference('Player_Info/' + str(koordinater))
+        self.ref = db.reference('koordinater/' + str(koordinater))
         self.a = self.ref.get()
         return self.a
 
     def get_all(self):
-        self.ref = db.reference('Player_Info/')
+        self.ref = db.reference('koordinater/')
         self.a = self.ref.get()
         return self.a
 
     def add(self, koordinater, visited = 1, active = 0):
-        self.ref = db.reference('Player_Info')
+        self.ref = db.reference('koordinater/')
         if not self.get(koordinater):
+            newKod = self.ref.child(str(koordinater))
+            newKod.update({
+                "visited" : visited,
+                "active" : active
+            })
+
             print("this koods arent therre")
 
             #ref.update({
@@ -34,7 +40,7 @@ class DBFunctions():
            # })
 
     def update(self, koordinater, key, value):
-        ref = db.reference('Player_Info/' + str(koordinater))
+        ref = db.reference('koordinater/' + str(koordinater))
         if self.get(koordinater):
             print("Update called found something to updpate")
 
@@ -42,10 +48,12 @@ class DBFunctions():
 
     def delete(self, koordinater):
         if self.get(id):
-            ref = db.reference('Player_Info/' + str(koordinater))
+            ref = db.reference('koordinater/' + str(koordinater))
             ref.delete()
 
 
 #DBFunctions().add(201082527824347136, "Dyberg")
 #DBFunctions().add(330761716386234369, "Rune")
 #DBFunctions().add(184896015218900992, "Lohmann")
+
+#DBFunctions().add(1235)
